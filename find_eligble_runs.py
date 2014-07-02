a510dfc0506b460665ed0a5e79b3814c0ed42b94
Tom Bair <tom.bair@gmail.com>
2014-07-02 14:48:18 -0500

    cleaned up find_eligble_runs and added write to done file

--- find_eligble_runs.py
+++ find_eligble_runs.py
@@ -15,19 +15,14 @@ def find_dirs(directory):
     return find_dirs_list
 
 
-def find_files(directory, pattern):
-    try:
-        found_files = os.listdir(directory)
-        for f in found_files:
-            basename = os.path.basename(f)
-            if basename == pattern:
-                yield os.path.join(directory, f)
-    except Exception, e:
-        logger.info('Got exception %s from find_files in %s' % (e, sys.argv[1]))
-        yield None
+def has_required_files(directory_to_check, list_of_files):
+    for file_to_check in list_of_files:
+        if not os.path.exists(os.path.join(directory_to_check,file_to_check)):
+            return False
+    return True
 
 
-def harvest_for_old_processes(file_to_check):
+def get_already_run(file_to_check):
     ds = []
     old_processes_file = open(file_to_check, 'r')
     for line in old_processes_file:
@@ -35,12 +30,18 @@ def harvest_for_old_processes(file_to_check):
         ds.append(line)
     return ds
 
+def append_to_already_run(directory_path,done_file_path):
+    append_fh = open(done_file_path, "a")
+    append_fh.write(directory_path)
+    append_fh.close()
+# Set up reading of config file
 config = ConfigParser.ConfigParser()
 config.readfp(open('pathway.cfg'))
 readDirectory = config.get('Globals', 'SeqDirectory')
 readySymDir = config.get('Globals', 'readyToRun')
-done = config.get('Globals', 'doneIgnore')
+done_directories = config.get('Globals', 'doneIgnore')
 
+# Set up logger to handle both info and warn levels
 logger = logging.getLogger(sys.argv[0])
 fh = logging.FileHandler(config.get('Globals', 'logfile'))
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -49,21 +50,28 @@ logger.addHandler(fh)
 logger.setLevel(logging.INFO)
 logger.info("Starting")
 
-old_processes = harvest_for_old_processes(done)
+
+old_processes = get_already_run(done_directories)
+
+if not os.path.exists(readDirectory):
+    logger.warn("Config file might be wrong cannot find read directory check Globals -> SeqDirectory")
+    sys.exit(1)
 dirs = find_dirs(readDirectory)
-count = 0
+
+count = 0 # counters to track what is being looked at and what has to be done yet
 total = 0 
+
 for d in dirs:
     d = os.path.join(readDirectory, d)
-    files = find_files(d, 'Basecalling_Netcopy_complete.txt')
-    for file in files:
-        if file is not None:
-            total += 1
-            if os.path.exists(os.path.join(d, 'ImageAnalysis_Netcopy_complete.txt')):
-                if os.path.dirname(file) not in old_processes:
-                    count += 1
-                    rtw = os.path.join(readySymDir, os.path.basename(os.path.dirname(file)))
-                    if not os.path.exists(rtw):
-                        os.symlink(os.path.dirname(file), rtw)
+    if d not in old_processes: # check to see if we have already processed this file
+        count += 1
+        if has_required_files(d,config.get('find_eligible','check_files')): # check to make sure it is ready
+            rtw = os.path.join(readySymDir, os.path.basename(os.path.dirname(file)))
+            if not os.path.exists(rtw):
+                os.symlink(os.path.dirname(file), rtw)
+                total += 1
+                #have linked the directory to the ready folder should now append it to the done file
+                append_to_already_run(d,done_directories)
+
 logger.info("Found %s directories that have data" % (total, ))
 logger.info("Found %s directories that need processed" % (count,))
