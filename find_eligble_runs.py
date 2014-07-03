638185cffdd01c3fabe02ca21c08c0ad2df4a38b
Tom Bair <tom.bair@gmail.com>
2014-07-03 12:58:59 -0500

    deleted file

--- find_eligble_runs.py
+++ find_eligble_runs.py
@@ -56,7 +56,7 @@ Checks to see if a directory has a single .csv file, if so it returns the path t
 
 def illumina_directory_form(d):
     print os.path.basename(d).split('_')
-    if len(os.path.dirname(d).split('_')) == 3:
+    if len(os.path.dirname(d).split('_')) == 4:
         if d.endswith('XX'):
             return True
     return False
