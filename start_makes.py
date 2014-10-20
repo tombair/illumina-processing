9293925a59bcb698273c16364ab43d79dfc0b68b
tombair <tom.bair@gmail.com>
2014-10-20 09:24:59 -0500

    make, remove nohup and write to file instead

--- start_makes.py
+++ start_makes.py
@@ -38,7 +38,7 @@ def in_progress_runs(array):
 
 def make_file(original_directory):
     os.chdir(original_directory)
-    process = subprocess.Popen(['nohup', 'make', '-j', '8'], stdout=subprocess.PIPE, shell=True)
+    process = subprocess.Popen(['make', '-j', '8'], stdout=open("nohup.out",'w'),stderr="make_errors", shell=True)
     out, err = process.communicate()
     logger.info("Started make it returned %s" % (err, ))
     return True
